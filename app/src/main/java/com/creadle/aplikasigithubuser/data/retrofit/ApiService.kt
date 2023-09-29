bbabc55350d47798aa65ba6526f3d47957d7a77b
MohammadUsmanAsegaf <usmanasegaf7@gmail.com>
2023-09-29 15:57:54 +0700

    aplikasi selesai, mudah mudahan dengan push ini tidak merusak tokennya, karena sudah sama token konensi android studio ini dengan token yang di generate :)

--- app/src/main/java/com/creadle/aplikasigithubuser/data/retrofit/ApiService.kt
+++ app/src/main/java/com/creadle/aplikasigithubuser/data/retrofit/ApiService.kt
@@ -13,25 +13,25 @@ import retrofit2.http.Query
 
 interface ApiService {
     @GET("search/users")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
     fun getSearchUsers(
         @Query("q") query: String
     ): Call<UserResponse>
 
     @GET("users/{username}")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
     fun getUserDetail(
         @Path("username") username: String
     ): Call<DetailUserResponse>
 
     @GET("users/{username}/followers")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
     fun getFollowers(
         @Path("username") username: String
     ): Call<ArrayList<User>>
 
     @GET("users/{username}/following")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
     fun getFollowing(
         @Path("username") username: String
     ): Call<ArrayList<User>>
